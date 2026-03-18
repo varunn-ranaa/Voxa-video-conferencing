c72c2e4592f7a472550365c5d8994093dc3f09d1
varunn_ranaa <varunrana1777@gmail.com>
2026-03-18 13:48:22 +0530

    landing page

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -5,30 +5,36 @@ import { Link } from 'react-router-dom';
 export default function landing() {
   return (
     <div className='landingPageContainer'>
-        <nav>
-            <div className='navHeader'>
-              <h2>VO<span className='logoAccent'>X</span>A <span className='logoMeet'>Meet</span></h2>
-            </div>
-            <div className='navList'>
-               <p>Join as Guest</p>
-               <p>Register</p>
-               <div className='loginBtn' role='button'>
-                  <p>Login</p>
-                </div>
-            </div>
-        </nav>
-
-        <div className='landingMainContainer'>
-            <div><h1><span style={{color : '#846af5'}}>Connect</span> without limits.</h1>
-              <p>Where great teams meet, decide, and deliver.</p>
-              <div role='button'>
-                <Link to={'/'}>Get Started</Link>
-               </div>
-            </div>
-            <div>
-                <img src="/mobile.png" alt="" style={{width : 450}}/>
-            </div>
+      <nav>
+        <div className='navHeader'>
+          <h2>VO<span className='logoAccent'>X</span>A <span className='logoMeet'>Meet</span></h2>
         </div>
+        <div className='navList'>
+          <p>Join as Guest</p>
+          <p>Register</p>
+          <div className='loginBtn' role='button'>
+            <p>Login</p>
+          </div>
+        </div>
+      </nav>
+
+      <div className='landingMainContainer'>
+
+        <div className='left'>
+          <div className='landBadge'>
+            <span className='badgeDot'></span>
+            Secure · Encrypted · Instant
+          </div>
+          <h1><span style={{ color: '#846af5' }}>Connect</span> without limits.</h1>
+          <p>Where great teams meet, decide, and deliver.</p>
+          <div role='button'>
+            <Link to={'/auth'}>Get Started</Link>
+          </div>
+        </div>
+        <div className='right'>
+          <img src="/mobile.png" alt="" style={{ width: 450 }} />
+        </div>
+      </div>
     </div>
   )
 }
