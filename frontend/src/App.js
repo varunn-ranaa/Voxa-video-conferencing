bbab1fc171c9b3d632d777e9023e0a8e4545fcc1
varunn_ranaa <varunrana1777@gmail.com>
2026-03-19 18:22:31 +0530

    Auth route added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import { Route,  BrowserRouter as Router, Routes } from 'react-router-dom';
 import LandingPage from './pages/landing'
+import Authentication from './pages/authentication';
+import { AuthProvider } from './contexts/authContext';
 
 function App() {
   return (
-     <>
+     <div className='App'>
        <Router>
+         <AuthProvider>
           <Routes>
              <Route  path='/' element={ <LandingPage/> }/>
+             <Route path='/auth' element={<Authentication/>}/>
           </Routes>
+        </AuthProvider>         
        </Router>
-     </>
+
+     </div>
   );
 }
 
