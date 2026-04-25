37b257cbe9bd53764eb5d6b51bda7022927e3135
varunn_ranaa <varunrana1777@gmail.com>
2026-04-25 21:10:01 +0530

    user-routes

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,12 +1,12 @@
 import {Router} from 'express';
-import { login , register } from '../controllers/user.controller.js';
+import { login , register , addToActivity , getAllActivity } from '../controllers/user.controller.js';
 
 
 const router = Router();
 
 router.route('/login').post(login);
 router.route('/register').post(register);
-router.route('/add_to_activity');
-router.route('/get_all_activity');
+router.route('/add_to_activity').post(addToActivity);
+router.route('/get_all_activity').get(getAllActivity);
 
 export default router;
